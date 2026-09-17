3d6b28caee4f56c0161f478f6d598086ad216c46
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2026-09-17 19:15:32 +0200

    update code for lambda functions to node22

--- pulumi/lambda/ondisconnect/app.js
+++ pulumi/lambda/ondisconnect/app.js
@@ -1,22 +1,17 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const AWS = require("aws-sdk");
-AWS.config.update({ region: process.env.AWS_REGION });
-const DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+const { DynamoDBClient, DeleteItemCommand } = require("@aws-sdk/client-dynamodb");
+const ddb = new DynamoDBClient({});
 
 const connectionsTable = process.env.CONNECTIONS_TABLE
 
 exports.handler = async function (event, context, callback) {
-  const deleteParams = {
-    TableName: connectionsTable,
-    Key: {
-      id: { S: event.requestContext.connectionId }
-    }
-  };
-
   try {
-    await DDB.deleteItem(deleteParams).promise();
+    await ddb.send(new DeleteItemCommand({
+      TableName: connectionsTable,
+      Key: { id: { S: event.requestContext.connectionId } }
+    }));
   } catch (e) {
     console.log("Error while disconnecting.", e)
     return { statusCode: 500, body: e.stack };
--- pulumi/lambda/sendmessage/app.js
+++ pulumi/lambda/sendmessage/app.js
@@ -1,17 +1,17 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const AWS = require('aws-sdk');
+const { DynamoDBClient, GetItemCommand, PutItemCommand, QueryCommand, DeleteItemCommand } = require("@aws-sdk/client-dynamodb");
+const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require("@aws-sdk/client-apigatewaymanagementapi");
 
-const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+const ddb = new DynamoDBClient({});
 
 const connectionsTable = process.env.CONNECTIONS_TABLE
 const gamesTable = process.env.GAMES_TABLE
 
 exports.handler = async (event, context) => {
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  const apigw = new ApiGatewayManagementApiClient({
+    endpoint: 'https://' + event.requestContext.domainName + '/' + event.requestContext.stage
   });
 
   const requestData = JSON.parse(event.body).data
@@ -19,18 +19,13 @@ exports.handler = async (event, context) => {
   console.log("Received request", requestData)
 
   if (requestData.action === 'connectToGame') {
-    const gameStateResponse = await ddb.getItem({
+    const gameStateResponse = await ddb.send(new GetItemCommand({
       TableName: gamesTable,
-      Key: { "id": {
-          S: requestData.gameId
-        }},
-      ProjectionExpression: 'game_state' }
-    ).promise();
+      Key: { "id": { S: requestData.gameId }},
+      ProjectionExpression: 'game_state'
+    }));
 
-    let gameState = {
-      id: null,
-      concept: []
-    }
+    let gameState = { id: null, concept: [] }
     if (gameStateResponse.Item) {
       gameState = JSON.parse(gameStateResponse.Item.game_state.S);
     }
@@ -39,22 +34,19 @@ exports.handler = async (event, context) => {
 
     const postData = JSON.stringify({
       mutation: 'newMessage',
-      data: {
-        action: 'newState',
-        state: gameState
-      }
+      data: { action: 'newState', state: gameState }
     });
 
-    await ddb.putItem({
+    await ddb.send(new PutItemCommand({
       TableName: connectionsTable,
       Item: {
         "id": { S: event.requestContext.connectionId },
         "game_id": { S: requestData.gameId }
       }
-    }).promise();
+    }));
 
     try {
-      await apigwManagementApi.postToConnection({ ConnectionId: event.requestContext.connectionId, Data: postData }).promise();
+      await apigw.send(new PostToConnectionCommand({ ConnectionId: event.requestContext.connectionId, Data: postData }));
     } catch(error) {
       console.log(`Error when sending game state to connection ${event.requestContext.connectionId}`, error);
       return { statusCode: 500, body: error };
@@ -63,25 +55,23 @@ exports.handler = async (event, context) => {
     return { statusCode: 200, body: 'Data sent.' };
   }
 
-  await ddb.putItem({
+  await ddb.send(new PutItemCommand({
     TableName: gamesTable,
     Item: {
       "id": { S: requestData.gameId },
       "game_state": { S: JSON.stringify(requestData.state) }
     }
-  }).promise();
+  }));
 
   let connectionData
   try {
-    connectionData = await ddb.query({
-      ExpressionAttributeValues: {
-        ':game_id' : {S: requestData.gameId}
-      },
+    connectionData = await ddb.send(new QueryCommand({
+      ExpressionAttributeValues: { ':game_id': { S: requestData.gameId } },
       KeyConditionExpression: 'game_id = :game_id',
       TableName: connectionsTable,
       IndexName: "game_id_index",
       ProjectionExpression: "id"
-    }).promise();
+    }));
   } catch (e) {
     console.log(`Error finding connections for game ${requestData.gameId}`, e)
     return { statusCode: 500, body: e.stack };
@@ -89,24 +79,21 @@ exports.handler = async (event, context) => {
 
   const postData = JSON.stringify({
     mutation: 'newMessage',
-    data: {
-      action: 'newState',
-      state: requestData.state
-    }
+    data: { action: 'newState', state: requestData.state }
   });
 
   const postCalls = connectionData.Items.map(async ({ id }) => {
     const connectionId = id.S;
     console.log(`Sending new game data to connection ${connectionId}`)
     try {
-      return await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      return await apigw.send(new PostToConnectionCommand({ ConnectionId: connectionId, Data: postData }));
     } catch (e) {
-      if (e.statusCode === 410) {
+      if (e.$metadata?.httpStatusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
-        return await ddb.deleteItem({
+        return await ddb.send(new DeleteItemCommand({
           TableName: connectionsTable,
-          Key: { "id" : { S: connectionId } }
-        }).promise();
+          Key: { "id": { S: connectionId } }
+        }));
       } else {
         throw e;
       }
